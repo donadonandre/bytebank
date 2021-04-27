1f5a1b27ad624e6953fef0496dcbdb9da5ee7ea4
André Donadon <andre.donadon@outlook.com>
2021-04-27 16:37:39 -0300

    Aplicando Labels e limpando o código

--- src/main.kt
+++ src/main.kt
@@ -6,7 +6,7 @@ fun main() {
     contaAndre.deposita(100.0)
     contaAndre.deposita(50.0)
 
-    val contaJoao = Conta("João", 1001)
+    val contaJoao = Conta(titular = "João", numero = 1001)
     contaJoao.deposita(200.0)
     contaJoao.saca(50.0)
     var efetuouTransferencia = contaJoao.transfere(50.0, contaAndre)
@@ -19,23 +19,10 @@ fun main() {
     println("Conta do ${contaJoao.titular} tem ${contaJoao.saldo}")
 }
 
-class Conta(var titular: String, var numero: Int) {
-//    var titular = titular
-//    var numero = numero
+class Conta(var titular: String, val numero: Int) {
+
     var saldo = 0.0
         private set
-
-    // O construtor pode ficar direto na definição.
-//    constructor(titular: String, numero: Int) {
-//        this.titular = titular
-//        this.numero = numero
-//    }
-
-//        já existe isso encapsulado
-//        set(valor) {
-//            field = valor
-//        }
-//        get(){return field}
 
     fun deposita(valorADepositar: Double) {
         saldo += valorADepositar
@@ -47,7 +34,6 @@ class Conta(var titular: String, var numero: Int) {
         }
     }
 
-//    fun transfere(valor: Double, destino: Conta): Unit { -> Esse é o padrão que não precisa digitar
     fun transfere(valor: Double, destino: Conta): Boolean {
         if (valor <= saldo) {
             this.saca(valor)
@@ -66,19 +52,3 @@ class Conta(var titular: String, var numero: Int) {
         }
     }
 }
-
-//fun executandoFors() {
-//    println("-----------")
-//    println("PRIMEIRO FOR")
-//
-//    for (i in 1..5) {
-//        println(i)
-//    }
-//
-//    println("-----------")
-//    println("SEGUNDO FOR")
-//
-//    for (i in 6 downTo 0 step 2) {
-//        println(i)
-//    }
-//}
