e8cde47a6f2d20967d084086b5800e545bd06ecf
André Donadon <andre.donadon@outlook.com>
2021-04-28 16:02:51 -0300

    Composição com alto e baixo acoplamento; Iniciando variaveis globais

--- src/br/com/donadonandre/bytebank/main.kt
+++ src/br/com/donadonandre/bytebank/main.kt
@@ -1,4 +1,16 @@
+import br.com.donadonandre.bytebank.modelo.Cliente
+import br.com.donadonandre.bytebank.modelo.ContaPoupanca
+
 fun main() {
+
+    var totalContas = 0
+
+    val andre = Cliente(nome="André",cpf="",senha=1)
+    ContaPoupanca(titular = andre, numero = 1000)
+    totalContas++
+
+    println("Total de contas: ${totalContas}")
+
 
 }
 
